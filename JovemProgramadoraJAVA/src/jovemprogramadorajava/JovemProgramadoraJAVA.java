40857cd7b7c7fb3163486537b174fc19ddf2835b
lucasbaldan <105757358+lucasbaldan@users.noreply.github.com>
2025-10-08 21:34:24 -0300

    Aula 1

--- JovemProgramadoraJAVA/src/jovemprogramadorajava/JovemProgramadoraJAVA.java
+++ JovemProgramadoraJAVA/src/jovemprogramadorajava/JovemProgramadoraJAVA.java
@@ -3,7 +3,16 @@ package jovemprogramadorajava;
 public class JovemProgramadoraJAVA {
 
     public static void main(String[] args) {
-
+        
+        String nome = "Lucas", sobrenome = "Baldan";
+        
+        int anoNascimento = 2002;
+        int anoAtual = 2025;
+        int idade;
+        
+        idade = anoAtual - anoNascimento; 
+        
         System.out.println("Ola mundo");
+        System.out.println("Você tem " + idade + " anos");
     }
 }
