1ed39156a1f77a3074e0c60cfe2c9ed43d78e4c7
lucasbaldan <105757358+lucasbaldan@users.noreply.github.com>
2025-10-15 21:39:44 -0300

    aula de condições e operadores

---
+++ JovemProgramadoraJAVA/src/jovemprogramadorajava/imc.java
@@ -0,0 +1,47 @@
+package jovemprogramadorajava;
+
+import java.util.Scanner;
+
+public class imc {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        double peso, altura, imc;
+        char sexo;
+
+        System.out.println("DIGITE O SEU GENERO - H PARA HOMEM E M PARA MULHER");
+        sexo = scan.next().charAt(0);
+
+        if (sexo != 'h' && sexo != 'H' && sexo != 'M' && sexo != 'm') {
+            System.err.println("VOCE DIGITOU UM SEXO INVALIDO");
+            System.exit(0);
+        }
+        
+        System.out.println("DIGITE SEU PESO");
+        peso = scan.nextDouble();
+
+        System.out.println("DIGITE SUA ALTURA");
+        altura = scan.nextDouble();
+
+        imc = peso / (altura * altura); 
+         
+        System.out.println("Seu imc calculado eh: " + imc);
+        if (sexo == 'M' || sexo == 'm') {
+            
+            if(imc > 39.0){
+                System.out.println("SEU INDICE EH OBESIDADE MORBIDA");
+            } else if(imc >= 30 && imc <= 39) {
+                System.out.println("SEU INDICE EH OBESIDADE MODERADA");
+            } else if(imc >= 25 && imc <= 29.9) {
+                System.out.println("SEU INDICE EH OBESIDADE LEVE");
+            } else if(imc >= 20 && imc <= 24.9) {
+                System.out.println("SEU INDICE EH NORMAL");
+            } else {
+                System.out.println("SEU INDICE EH ABAIXO DO NORMAL");
+            }
+        }
+
+    }
+
+}
